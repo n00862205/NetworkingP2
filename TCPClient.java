27b6b43df1b691c19ae2e25ae07a829d23326990
n00955820 <n00955820@ospreys.unf.edu>
2016-04-10 16:36:58 -0400

    Update TCPClient.java

--- TCPClient.java
+++ TCPClient.java
@@ -3,6 +3,21 @@ import java.net.*;
 import java.util.Scanner;
 
 //host name is 192.168.100.111
+/***********************************************************
+ * Project 2
+ * TCP Client
+ * Group Members:  Karam Safar (n00862205), 
+ * 				   Terrance Santilli (n00955820), 
+ * 				   Stephanie Rector (n00896243), 
+ * 				   Amanda Davis (n00850605),
+ * 				   Fadi Safar (),
+ * 				   Marco del Castillo ()
+ * 
+ * Purpose: The purpose of this project is to simulate a client
+ * 	connection to a server where the client has the ability to 
+ * 	send a request and wait for the server to respond. 
+ * 
+ ***********************************************************/
 class TCPClient
 {
    public static void main(String argv[]) throws Exception
@@ -43,52 +58,61 @@ class TCPClient
             
             //Get user Input
             Option = inFromUser.nextLine();
-            
-            System.out.println("Enter Number of Threads");
-         	
-         	//Get number of threads
-            num = inFromUser2.nextInt();
+           
             
             if(Option.equals("1") || Option.equals("2") || Option.equals("3") || Option.equals("4")
             		|| Option.equals("5") || Option.equals("6"))
             {
+                System.out.println("Enter Number of Threads");
+             	//Get number of threads
+                num = inFromUser2.nextInt();
+                
+            	//construct a thread timer class
             	threadTime time = new threadTime(num);
-            	MyThread thread[] = new MyThread[75];
+            	//create an array to hold all threads
+            	MyThread thread[] = new MyThread[num];
             	
+            	//loop through a thread array to start each thread
    				for(int i = 0; i < num; i++) {
-   				
-   					// Create a new thread
+   					//create a new thread
    					thread[i] = new MyThread(outToServer, inFromServer, Option, time);
    					thread[i].start();
-   					
    				}
    				
+   				//loop through the thread array to wait for threads to terminate and
+   				//print the average time for the threads.
    				for(int i = 0; i < num; i++)
    				{
-   					// Waits for all threads
+   					// Waits for all threads to finish
    					try	{
    						thread[i].join();
    					} catch(InterruptedException e) {
    						System.out.println(e);
    					}
-                  counter = i + 1;
-                  if(num == 1)
-                     System.out.println(thread[i].getResults());
-                  
-   					if ( (counter % 5 == 0) || (counter == 1) ) {
+   					
+   					counter = i + 1;
+   					if(num == 1)
+   						System.out.println(thread[i].getResults());
+   					
+   					//get the average time for 1,5,10,15.... threads
+   					if ((counter % 5 == 0) || (counter == 1) ) {
    						avgTime = time.getAverage(counter);
    						System.out.println("Average time at " + counter + " threads (in milliseconds): " + avgTime);
    					}
    				}
             } else if(Option.equals("7")) {
-               outToServer.println(Option);
-               break;
+            	//send the the option to the server so it terminates
+            	outToServer.println(Option);
+               	break;
             }
             else
-               System.out.println("Invalid Input");
+            	System.out.println("Invalid Input");
          }
          
+         //variable clean-up
          clientSocket.close();
+         outToServer.close();
+         inFromServer.close();
          inFromUser.close();
          inFromUser2.close();
       }
@@ -100,26 +124,30 @@ class TCPClient
 }
 
 class MyThread extends Thread {
-   private PrintWriter outToServer;
-   private BufferedReader inFromServer;
-   private String Option;
-   private threadTime time;
-   private String FullResults;
-
-   public MyThread(PrintWriter ots, BufferedReader ifs, String o, threadTime t){
-      this.outToServer = ots;
-      this.inFromServer = ifs;
-      this.Option =  o;
-      this.time = t;
-   }
-  
-   public void run(){
-	   long totalTime;
+	private PrintWriter outToServer;
+	private BufferedReader inFromServer;
+	private String Option;
+	private threadTime time;
+	private String FullResults;
+	
+	//constructor 
+	public MyThread(PrintWriter ots, BufferedReader ifs, String o, threadTime t){
+		this.outToServer = ots;
+		this.inFromServer = ifs;
+		this.Option =  o;
+		this.time = t;
+	}
+	
+	//
+	public void run(){
+		long totalTime;
 	  	long startTime;
 		long endTime;
 		
+		//start timer
 		startTime = System.currentTimeMillis();
 		
+		//send the selected option to the server
 		try {
 			outToServer.println(Option);
 		} 
@@ -128,8 +156,13 @@ class MyThread extends Thread {
 		}
 		
 		try {
+			//get the results from the server
 			FullResults = inFromServer.readLine();
+			
+			//stop the timer
 			endTime = System.currentTimeMillis();
+			
+			//calculate and store the total time
 			totalTime = endTime - startTime;
 			time.add(totalTime);
 		} catch(IOException e) {
@@ -146,18 +179,20 @@ class MyThread extends Thread {
 
 class threadTime {
 	long[] times;
-   int counter;
+	int counter;
 
+	//constructor
 	public threadTime(int n) {
-      times = new long[n];
+		times = new long[n];
 		counter = 0;
 	}
 
+	//add the total time to the times array
 	public synchronized void add (long t) {
 		times[counter++] = t;
 	}
 	
-	
+	//get the average time for a number of threads
 	public double getAverage (int num) {
 		double avg = 0;
 		
